59382d8a74da81cb517c8ab71303e82bd2162582
lou-aceved00 <louaceved00@gmail.com>
2026-08-04 09:52:14 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+let cliente = prompt("Escribe tu nombre")
+console.log("Hola " + cliente); 
+
+let numA = parseInt(prompt("Ingrese el primer numero"));
+let numB = parseInt(prompt("Ingrese el segundo numero"));
+let resultado = numA * numB;
+
+alert("El resultado de la multiplicacion entre sus numeros es: " +resultado);
+
+
